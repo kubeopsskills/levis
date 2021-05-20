445b062902805e275c512c738fce4cc52e0fe407
dmakeroam <cpfapp@gmail.com>
2021-05-20 13:58:25 +0700

    fix: Remove Fs-extra as it's not compatible with pkg

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,12 +1,20 @@
 import * as log4js from "log4js";
-import * as FsExtra from "fs-extra"
+import * as Fs from "fs"
 
 const log = log4js.getLogger("FileUtils");
 log.level = "info";
 
 export class FileUtils {
     public static Move(oldPath: string, newPath: string): void  {
-        FsExtra.move(oldPath, newPath)
+        if (newPath.includes("/")){
+            const newPathPrefix: string = newPath.split("/")[0]
+            if(Fs.existsSync(newPathPrefix)){
+                Fs.rmdirSync(newPathPrefix, { recursive: true });
+            }
+            Fs.mkdirSync(newPathPrefix)
+        }
+        Fs.copyFileSync(oldPath, newPath);
+        Fs.unlinkSync(oldPath);
         log.info("Successfully moved %s to %s.", oldPath, newPath);
     }
 }
