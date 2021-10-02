5a379c9c1438045509f46191ea1883df83ef74d6
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-10-02 22:04:06 +0700

    feat: use fs-extra to move file, fix cannot build

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,21 +1,17 @@
 import * as log4js from "log4js";
-import * as Fs from "fs"
+import * as Fs from "fs-extra"
 
 const log = log4js.getLogger("FileUtils");
 log.level = "info";
 
 export class FileUtils {
     public static Move(oldPath: string, newPath: string): void  {
-        if (newPath.includes("/")){
-            const newPathPrefix: string = newPath.split("/")[0]
-            if(!Fs.existsSync(newPathPrefix)){
-                Fs.mkdirSync(newPathPrefix)
-            }
+        try {
+            Fs.moveSync(oldPath, newPath, { overwrite: true });
+            log.info("Successfully moved %s to %s.", oldPath, newPath);
+        } catch (err) {
+            log.error("Cannot move file with error : %s", err);
+            throw err;
         }
-        Fs.copyFileSync(oldPath, newPath);
-        Fs.unlinkSync(oldPath);
-        const oldPathPrefix: string = oldPath.split("/")[1]
-        Fs.rmdirSync(oldPathPrefix);
-        log.info("Successfully moved %s to %s.", oldPath, newPath);
     }
 }
