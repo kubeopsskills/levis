675899f6f245f7f7fa4695d83a3b78e491c6e1f1
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-10-03 18:10:52 +0700

    feat: setup deploy

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "roots": [
-        "<rootDir>"
-    ],
-    testMatch: [ '**/*.test.ts'],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-}
+  roots: ["<rootDir>"],
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
+};
