fa99ef779a43bac55c288bd84d8cbbeae4e5b66a
creampxxz <naphun.m@kubeops.guru>
2022-03-15 16:54:34 +0700

    refactor: change path to constant

--- src/services/createCommand.ts
+++ src/services/createCommand.ts
@@ -1,7 +1,7 @@
 import * as log4js from "log4js";
 import * as Minimist from "minimist";
 import ILevisCommand from "../interfaces";
-import { Command } from "../models";
+import { Command , Path } from "../models";
 import { Message } from "../levis/message";
 import MicroServiceChart from "../charts";
 import { App } from "cdk8s";
@@ -12,15 +12,18 @@ const log = log4js.getLogger();
 export class CreateCommand implements ILevisCommand {
     
     private command!: Command;
-    private app!: App;
+    private app: App;
 
+    constructor(app: App){
+        this.app = app;
+    }
 
     init(args: Minimist.ParsedArgs): Command {
         log.debug("CreateCommand");
         // get a value from -f  
         const inputFilePath=args.f;
         // get a value from -o
-        const outputFilePath=args.o ? args.o: "manifests/levis.k8s.yaml";
+        const outputFilePath=args.o ? args.o: Path.OUTPUT_PATH;
         
         // Message handler if with -f flag
         if(!inputFilePath){
@@ -41,7 +44,7 @@ export class CreateCommand implements ILevisCommand {
         new MicroServiceChart(this.app, this.command);
         // Activate CDK8S for generating yaml file
         this.app.synth();
-        FileUtils.Move("./dist/levis.k8s.yaml", this.command.outputFilePath);
+        FileUtils.Move(Path.CURRENT_PATH, this.command.outputFilePath);
     }
 
 }
